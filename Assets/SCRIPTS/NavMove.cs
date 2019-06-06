82adde1554eae26b04d980fb4dbc9fe0f7eb6171
Maxwell Bake <20.maxwell.dickes@ksd.org>
2019-06-06 13:18:30 -0700

    npc interactions

--- Assets/SCRIPTS/NavMove.cs
+++ Assets/SCRIPTS/NavMove.cs
@@ -8,6 +8,8 @@ public class NavMove : MonoBehaviour
     NavMeshAgent Agent;
     public GameObject Player;
     public float chaseDistance = 10.0f;
+    public float attackDistance = 2.0f;
+    public float attackTimer = 0.0f;
     private Vector3 Home;
     // Start is called before the first frame update
     void Start()
@@ -21,13 +23,25 @@ public class NavMove : MonoBehaviour
     void Update()
     {
         Vector3 direction = Player.transform.position - transform.position;
-        if (direction.magnitude <= chaseDistance)
+        if (direction.magnitude <= chaseDistance && direction.magnitude >= attackDistance)
         {
             Agent.destination = Player.transform.position;
         }
-        else
+        else if (direction.magnitude <= attackDistance)
+        {
+            //fire or swing
+            //direction = transform.position;
+            Agent.destination = transform.position;
+            attackTimer += Time.deltaTime;
+        }
+        else 
         {
             Agent.destination = Home;
+        }
+        if (attackTimer >= 1.5f)
+        {
+            Player.GetComponent<Health>().HP -= 1;
+            attackTimer = 0f;
         }
     }
 }
